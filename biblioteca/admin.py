6140cb4620f682a4eb31e2c1bb8b98c8ee715fc9
Juan Diego <vasquezfloresluisantonio33@gmail.com>
2026-09-19 19:10:24 -0500

    Personalizar con ModelAdmin:  list_display, search_fields

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Libro, Autor, FichaTecnica, Lector, Prestamo
 
-admin.site.register(Libro)
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "autor", "isbn", "precio", "disponible")
+    search_fields = ("titulo", "isbn")
+
+
+@admin.register(Prestamo)
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = ("libro", "lector", "fecha_prestamo", "fecha_devolucion_esperada", "estado")
+    search_fields = ("libro__titulo", "lector__nombre")
+
+
 admin.site.register(Autor)
 admin.site.register(FichaTecnica)
 admin.site.register(Lector)
-admin.site.register(Prestamo)
